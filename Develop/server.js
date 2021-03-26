a003260549b79efa1b60f1d6dc014d7a401a9ecc
fkibria <fkibria02@gmail.com>
2021-03-25 23:44:25 -0400

    updating

--- Develop/server.js
+++ Develop/server.js
@@ -15,17 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
-
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 
 app.use(require("./routes/api-routes"));

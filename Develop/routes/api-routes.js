9ebbc78e95e6c9a92e32c53dd5453f87913c11ab
fkibria <fkibria02@gmail.com>
2021-03-25 23:27:56 -0400

    deploying

---
+++ Develop/routes/api-routes.js
@@ -0,0 +1,64 @@
+let db = require("../models/workoutmodel");
+let router = require("express").Router();
+
+router.get("/api/workouts", (req, res) => {
+
+  db.aggregate([{
+          $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+      }, ])
+      .then((data) => {
+          res.json(data);
+      })
+      .catch((err) => {
+          res.status(400).json(err);
+      });
+});
+
+router.post("/api/workouts", ({ body }, res) => {
+
+  db.create(body)
+      .then((dbWorkout) => {
+          res.json(dbWorkout);
+      })
+      .catch((err) => {
+          res.status(400).json(err);
+      });
+});
+
+router.post("/api/workouts/:id", ({body}, res) => {
+  db.create(body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    })
+})
+
+router.put("/api/workouts/:id", (req, res) => {
+
+  db.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body })
+      .then((dbWorkout) => {
+          res.json(dbWorkout);
+      })
+      .catch((err) => {
+          res.status(400).json(err);
+      });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+
+    db.aggregate([{
+            $addFields: { totalDuration: { $sum: "$exercises.duration" } },
+        }])
+        .sort({ day: -1 })
+        .limit(7)
+        .then((data) => {
+            res.json(data.reverse());
+        })
+        .catch((err) => {
+            res.status(400).json(err);
+        });
+});
+
+module.exports = router;
---
+++ Develop/server.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const path = require("path");
+// const workoutModel = require("./models/workoutmodel")
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workout-tracker',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
+
+
+
+app.use(require("./routes/api-routes"));
+app.use(require("./routes/html-routes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
